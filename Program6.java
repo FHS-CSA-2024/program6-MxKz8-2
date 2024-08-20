165e79998c5497e33a5eaf481113e2d5cf9130a8
MxKz8-2 <kyletan555@gmail.com>
2024-08-20 09:28:32 -0700

    Update Program6.java
    Completed program 6

--- Program6.java
+++ Program6.java
@@ -1,7 +1,7 @@
 //import stuff here!
 import java.util.Scanner;
 //Your code here
-public class Program5 {
+public class Program6 {
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         
@@ -11,25 +11,36 @@ public class Program5 {
         System.out.println("Enter your radius:");
         radius = myScanner.nextDouble();
         
-        double diameter = 2 * radius;
-        diameter = Math.round(diameter*10.0*10.0)/100.0;
+        double diameter = 2.0 * radius;
+        diameter = (double) Math.round(diameter*10.0*10.0*10.0)/1000.0;
         
-        double area = radius * radius * pi;
-        area = Math.round(area*10.0*10.0)/100.0;
+        double area = (double) pi * Math.pow(radius, 2);
+        area = (double) Math.round(area*10.0*10.0*10.0)/1000.0;
         
-        double circumference = 2 * radius * pi;
-        circumference = Math.round(circumference*10.0*10.0)/100.0;
+        double circumference = 2.0 * radius * pi;
+        circumference = (double) Math.round(circumference*10.0*10.0*10.0)/1000.0;
         
         System.out.println("The Radius of the circle = " + radius);
         System.out.println("The Diameter of the circle = " + diameter);
         System.out.println("The Area of the circle = " + area);
         System.out.println("The Circumference of the circle = " + circumference);
         
-        
+        System.out.println();
+        System.out.println("This program was written by:");
+        System.out.println("Kyle Tan on 8/20/24 in 1st period");
     }
 }
 //Paste console output below:
 /*
 
+Enter your radius:
+3.712
+The Radius of the circle = 3.712
+The Diameter of the circle = 7.424
+The Area of the circle = 43.288
+The Circumference of the circle = 23.323
+
+This program was written by:
+Kyle Tan on 8/20/24 in 1st period
 
 */
